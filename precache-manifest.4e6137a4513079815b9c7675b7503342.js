837fc1252875b04ecaf8e44e88ac30b5005e7cb2
Tiago Nogueira <tiago.nogueira@farfetch.com>
2019-08-20 12:33:15 +0100

    Updates

---
+++ precache-manifest.4e6137a4513079815b9c7675b7503342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee4f4b11f31503c25f64576d19aaf19",
+    "url": "/nif-generator/index.html"
+  },
+  {
+    "revision": "24cc37c5b54afd70f0f2",
+    "url": "/nif-generator/static/css/main.31b27988.chunk.css"
+  },
+  {
+    "revision": "db359458f7ff8fc428ad",
+    "url": "/nif-generator/static/js/2.b4036808.chunk.js"
+  },
+  {
+    "revision": "24cc37c5b54afd70f0f2",
+    "url": "/nif-generator/static/js/main.ccd52754.chunk.js"
+  },
+  {
+    "revision": "d32e83bf9d0776152390",
+    "url": "/nif-generator/static/js/runtime~main.a77cd357.js"
+  },
+  {
+    "revision": "d25b0223375e528fc70e928b7393f86c",
+    "url": "/nif-generator/static/media/logo.d25b0223.png"
+  }
+]);
