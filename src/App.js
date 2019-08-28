97e8f74b20ff5583f5827cd7119c54eeb3255252
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 13:57:00 +0000

    Deploy rtnogueira/nif-generator to github.com/rtnogueira/nif-generator.git:gh-pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './util/theme';
+import { createBrowserHistory } from "history";
 
 // Redux
 import { Provider } from 'react-redux';
@@ -17,7 +19,20 @@ import NifGenerator from './pages/NifGenerator';
 
 const theme = createMuiTheme(themeFile);
 
+ReactGA.initialize("UA-146599861-1");
+
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
 class App extends Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={theme}>
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga';
 
+jest.mock('react-ga');
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
