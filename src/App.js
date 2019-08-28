dc7738b363d72ad0cd3d7113250cff7a9d041ea7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 10:38:36 +0000

    Deploy rtnogueira/nif-generator to github.com/rtnogueira/nif-generator.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
+
+// Components
+import Navbar from './components/Navbar';
+
+// Pages
+import NifGenerator from './pages/NifGenerator';
+
+const theme = createMuiTheme(themeFile);
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <HashRouter basename='/'>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={NifGenerator} />
+                <Route component={NifGenerator} />
+              </Switch>
+            </div>
+          </HashRouter>
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
