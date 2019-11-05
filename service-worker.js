234669bcad10b988ddf007c883b5ca3ffdbe1d82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-05 18:32:35 +0000

    Deploy rtnogueira/nif-generator to github.com/rtnogueira/nif-generator.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nif-generator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
