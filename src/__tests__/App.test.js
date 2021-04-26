efc5e6b88be12481e14eb718fa3886254c94c308
Tiago Nogueira <tiago.nogueira@farfetch.com>
2021-04-26 14:58:04 +0100

    feat: Change color scheme

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import ReactGA from 'react-ga';
+import App from '../App';
 
 jest.mock('react-ga');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
