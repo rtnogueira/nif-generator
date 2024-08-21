5995be86037b73bc82e6bbb22a36cc7be55dd074
Tiago Nogueira <rubentiagonogueira@gmail.com>
2024-08-21 18:46:58 +0100

    feat: Add google adsense

--- src/components/copy-to-clipboard/CopyToClipboard.js
+++ src/components/copy-to-clipboard/CopyToClipboard.js
@@ -1,7 +1,6 @@
 import FileCopy from '@material-ui/icons/FileCopy';
 import IconButton from '@material-ui/core/IconButton';
 import CheckCircle from '@material-ui/icons/CheckCircle';
-import PropTypes from 'prop-types';
 import useCopy from '../../shared/hooks/useCopy';
 import React from 'react';
 
@@ -13,13 +12,6 @@ const CopyToClipboard = ({ value }) => {
             {isCopied ? <CheckCircle /> : <FileCopy /> }
         </IconButton>
     );
-};
-
-CopyToClipboard.propTypes = {
-    value: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-    ]).isRequired,
 };
 
 export default CopyToClipboard;
--- src/shared/hooks/useCopy.js
+++ src/shared/hooks/useCopy.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import copyToClipboard from 'copy-to-clipboard';
 
 const useCopy = (textToCopy, successDuration = 1000) => {
@@ -22,11 +21,6 @@ const useCopy = (textToCopy, successDuration = 1000) => {
             setIsCopied(true);
         },
     ];
-};
-
-useCopy.propTypes = {
-    textToCopy: PropTypes.string.isRequired,
-    successDuration: PropTypes.number,
 };
 
 export default useCopy;
